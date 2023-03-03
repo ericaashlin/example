4313da7b5c928448237e54f98c34356e90c59265
ericaashlin <ashlin@fgilservices.com>
2023-03-03 12:33:07 +0530

    change

--- collection.java
+++ collection.java
@@ -2,9 +2,10 @@ import java.util.*;
 class TestJavaCollection1{  
 public static void main(String args[]){  
 ArrayList<String> list=new ArrayList<String>();//Creating arraylist  
-list.add("Ravi");//Adding object in arraylist  
-list.add("Vijay");  
-list.add("Ravi");  
+list.add("lulu");//Adding object in arraylist  
+list.add("victoria");  
+list.add("Ravi"); 
+list.add("bob");  
 list.add("Ajay");  
 //Traversing list through Iterator  
 Iterator itr=list.iterator();  
